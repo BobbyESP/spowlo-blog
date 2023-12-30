c664bf5508e29e717684bb7b84374a17b7b398e9
Gabriel Fontán <gabilessto@gmail.com>
2023-12-30 15:47:13 +0100

    Update site configuration and social image URL

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'BobbyESP', // Site author
+	title: 'Spowlo blog', // Site title.
+	description: 'What is behind the development of this downloader app.', // Description to display in the meta tags
+	lang: 'en-US',
+	ogLocale: 'en_US',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
