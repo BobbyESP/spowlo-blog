576d76eab133dd7971c0d39162e2b3c7464aeded
Gabriel Fontán <gabilessto@gmail.com>
2023-12-30 14:33:38 +0100

    Added introduction post. Delete unused images and update blog title.

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
-] as const
+export const CATEGORIES = ['General', 'Develpoment', 'Ideas', 'Personal'] as const
